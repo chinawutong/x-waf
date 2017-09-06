875144c5dbe67ab5aa10ad15a1b5575cdfa89319
shawn <qiusct@gmail.com>
2017-09-06 03:59:39 +0000

    add waf.conf

--- deploy/setting.sh
+++ deploy/setting.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
 
-    lua_package_path "/usr/local/openresty/nginx/conf/x-waf/?.lua;/usr/local/lib/lua/?.lua;;";
-    lua_shared_dict limit 100m;
-    lua_shared_dict badGuys 100m;
-    # cache for lua
-    lua_code_cache on;
-
-    init_by_lua_file /usr/local/openresty/nginx/conf/x-waf/init.lua;
-    access_by_lua_file /usr/local/openresty/nginx/conf/x-waf/access.lua;
